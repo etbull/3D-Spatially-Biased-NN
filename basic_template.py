e951a0b01f916e241ce458d332ac8c40a565d940
Ethan Turnbull <ethanwono@gmail.com>
2026-01-03 23:52:41 +0800

    First run where model worked

--- basic_template.py
+++ basic_template.py
@@ -21,15 +21,14 @@ class Model(nn.Module):
     def __init__(self, input_length, num_classes):
         super(Model, self).__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+            nn.Linear(input_length, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Linear(512, num_classes),
         )
         
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
